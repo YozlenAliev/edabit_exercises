4fd5dae07c4543269c1f4316f7f9dd8a7ff4c862
Yozlen Aliev <yozlenaliev@gmail.com>
2025-06-10 11:57:41 +0300

    Added files find_perimeter_of_rectangle.py and return_next_number.py

---
+++ exercises/find_perimeter_of_rectangle.py
@@ -0,0 +1,4 @@
+def find_perimeter(length: int, width: int) -> int:
+    return length * 2 + width * 2
+
+print(find_perimeter(20,10))
---
+++ exercises/return_next_number.py
@@ -0,0 +1,5 @@
+def addition(num: int) -> int:
+    return num + 1
+
+
+print(addition(9))
